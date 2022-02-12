6ae0801cab5d25cd7e261bd00397275817be812e
kurone02 <56928903+kurone02@users.noreply.github.com>
2022-02-12 18:11:49 +0700

    User utilities

---
+++ user.py
@@ -0,0 +1,7 @@
+# player.py
+
+class User:
+    def __init__(self, words_space: list=[], guess_history: list=[]):
+        self.words_space = words_space
+        self.guess_history = guess_history
+        pass
